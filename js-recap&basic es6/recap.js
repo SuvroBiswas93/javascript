9cca6b216f3f1e2609784db3bade023c297fa192
Suvro Biswas <shuvo.249963@gmail.com>
2026-02-03 14:17:22 +0600

    Practice dafault parameter for multiplication and practice arrw function

--- js-recap&basic es6/recap.js
+++ js-recap&basic es6/recap.js
@@ -8,7 +8,7 @@
 
 
 // default parameter
-
+// this is for addition
 function sum (num1 = 0, num2 = 0){
     total = num1 + num2 ;
     return console.log('the sum of two number is',num1,num2,total)
@@ -17,3 +17,19 @@ function sum (num1 = 0, num2 = 0){
 // sum(10,0) 
 sum(10) 
 
+// this is for multiplication
+
+function multiply (num1 = 1, num2 = 1){
+    total = num1 * num2 ;
+    return console.log('the multiply of two number is',num1,num2,total)
+}
+
+// sum(10,0) 
+multiply(10) 
+
+
+
+const add = (a,b)=>{
+    return a+b;
+}
+console.log(add(5,7))
